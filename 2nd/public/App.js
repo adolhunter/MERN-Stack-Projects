d8745a4bc8e608cfd20cf17af0e882fafbfa317e
adolhunter <mingshiwang123@gmail.com>
2020-03-25 03:03:50 -0500

    add ie support

--- 2nd/public/App.js
+++ 2nd/public/App.js
@@ -1,4 +1,16 @@
-const element = /*#__PURE__*/React.createElement("div", {
+"use strict";
+
+/* const element = (
+    <div title='Outer div'>
+        <h1>Hello World!</h1>
+    </div>
+); */
+var continents = ['africa', 'asia', 'america', 'australia', 'europe'];
+var helloContinents = Array.from(continents, function (c) {
+  return "Hello ".concat(c, "!");
+});
+var message = helloContinents.join(' ');
+var element = /*#__PURE__*/React.createElement("div", {
   title: "Outer div"
-}, /*#__PURE__*/React.createElement("h1", null, "Hello World!"));
+}, /*#__PURE__*/React.createElement("h1", null, message));
 ReactDOM.render(element, document.getElementById('contents'));
--- 2nd/src/App.jsx
+++ 2nd/src/App.jsx
@@ -1,6 +1,18 @@
-const element = (
+/* const element = (
     <div title='Outer div'>
         <h1>Hello World!</h1>
     </div>
+); */
+
+const continents = ['africa', 'asia', 'america', 'australia', 'europe']
+
+const helloContinents = Array.from(continents, c => `Hello ${c}!`);
+const message = helloContinents.join(' ');
+
+const element = (
+    <div title='Outer div'>
+        <h1>{message}</h1>
+    </div>
 );
+
 ReactDOM.render(element, document.getElementById('contents'));
