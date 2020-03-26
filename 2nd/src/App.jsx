a538c1671cd95828d8bd0bca71528f57b48fb1f0
adolhunter <mingshiwang123@gmail.com>
2020-03-25 23:13:13 -0500

    add issue tracker boilerplate

--- 2nd/src/App.jsx
+++ 2nd/src/App.jsx
@@ -1,4 +1,4 @@
-class HelloWorld extends React.Component{
+/* class HelloWorld extends React.Component{
     render(){
         const continents = ['Africa', 'America','Asia','Australia','Europe'];
         const helloContinents = Array.from(continents, c => `Hello ${c}!`);
@@ -12,5 +12,49 @@ class HelloWorld extends React.Component{
 }
 
 const element = <HelloWorld />;
+
+ReactDOM.render(element, document.getElementById('contents')); */
+
+
+class IssueFilter extends React.Component {
+    render() {
+        return (
+            <div>This is a placeholder for the IssueFilter.</div>
+        );
+    }
+}
+
+class IssueTable extends React.Component {
+    render() {
+        return (
+            <div>This is a placeholder for the issues.</div>
+        );
+    }
+}
+
+class IssueAdd extends React.Component {
+    render() {
+        return (
+            <div>This is a placeholder for the IssueAdd.</div>
+        );
+    }
+}
+
+class IssueList extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <h1>Issue Tracker</h1>
+                <IssueFilter />
+                <hr />
+                <IssueTable />
+                <hr />
+                <IssueAdd />
+            </React.Fragment>
+        );
+    }
+}
+
+const element = <IssueList />;
 
 ReactDOM.render(element, document.getElementById('contents'));
