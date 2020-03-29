53b7aa9b707494bf4bd500fe8713aafec2902fbc
adolhunter <mingshiwang123@gmail.com>
2020-03-29 00:04:23 -0500

    add query variables

--- 2nd/public/App.js
+++ 2nd/public/App.js
@@ -257,7 +257,7 @@ var IssueList = /*#__PURE__*/function (_React$Component3) {
           while (1) {
             switch (_context2.prev = _context2.next) {
               case 0:
-                query = "mutation {\n        issueAdd(issue: {\n            title: \"".concat(issue.title, "\",\n            owner: \"").concat(issue.owner, "\",\n            due: \"").concat(issue.due.toISOString(), "\",\n        }) {\n            id\n        }\n    }");
+                query = "mutation issueAdd($issue:IssueInputs!) {\n        issueAdd(issue: $issue) {\n            id\n        }\n    }";
                 _context2.next = 3;
                 return fetch("/graphql", {
                   method: "post",
@@ -265,7 +265,10 @@ var IssueList = /*#__PURE__*/function (_React$Component3) {
                     "Content-Type": "application/json"
                   },
                   body: JSON.stringify({
-                    query: query
+                    query: query,
+                    variables: {
+                      issue: issue
+                    }
                   })
                 });
 
--- 2nd/src/App.jsx
+++ 2nd/src/App.jsx
@@ -165,12 +165,8 @@ class IssueList extends React.Component {
   }
 
   async createIssue(issue) {
-    const query = `mutation {
-        issueAdd(issue: {
-            title: "${issue.title}",
-            owner: "${issue.owner}",
-            due: "${issue.due.toISOString()}",
-        }) {
+    const query = `mutation issueAdd($issue:IssueInputs!) {
+        issueAdd(issue: $issue) {
             id
         }
     }`;
@@ -178,7 +174,7 @@ class IssueList extends React.Component {
     const response = await fetch("/graphql", {
       method: "post",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ query })
+      body: JSON.stringify({ query, variables: { issue } })
     });
     this.loadData();
   }
