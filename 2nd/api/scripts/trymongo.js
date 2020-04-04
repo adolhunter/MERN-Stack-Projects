3b72e8080dec2879b0511b0eb176a2a297529bbb
adolhunter <mingshiwang123@gmail.com>
2020-04-04 17:43:44 -0500

    modify trymongo.js to use dotenv

--- 2nd/api/scripts/trymongo.js
+++ 2nd/api/scripts/trymongo.js
@@ -1,62 +1,70 @@
+require('dotenv').config();
 const { MongoClient } = require('mongodb');
-const url = 'mongodb://localhost/issuetracker';
+
+const url = process.env.DB_URL || 'mongodb://localhost/issuetracker';
 
 function testWithCallbacks(callback) {
-    console.log('\n--- testWithCallbacks ---');
-    const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
-    client.connect((err, client) => {
-        if (err) {
-            callback(err);
-            return;
-        }
-        const db = client.db();
-        const collection = db.collection('employees');
-
-        const employee = { id: 1, name: "A. Callback", age: 23 };
-        collection.insertOne(employee, (err, result) => {
-            if (err) {
-                client.close();
-                callback(err);
-                return;
-            }
-            console.log('result of insertion: \n', result.insertedId);
-            collection.find({ _id: result.insertedId }).toArray((err, docs) => {
-                if (err) {
-                    client.close();
-                    callback(err);
-                    return;
-                }
-                console.log('result of find: \n', docs);
-                client.close();
-                callback(err);
-            })
-        })
-    });
-}
-
-testWithCallbacks(err => {
+  console.log('\n--- testWithCallbacks ---');
+  const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+  client.connect(function(err, client) {
     if (err) {
-        console.log(err);
+      callback(err);
+      return;
     }
-    testWithAsync();
-})
+    console.log('Connected to MongoDB URL', url);
+
+    const db = client.db();
+    const collection = db.collection('employees');
+
+    const employee = { id: 1, name: 'A. Callback', age: 23 };
+    collection.insertOne(employee, function(err, result) {
+      if (err) {
+        client.close();
+        callback(err);
+        return;
+      }
+      console.log('Result of insert:\n', result.insertedId);
+      collection.find({ _id: result.insertedId})
+        .toArray(function(err, docs) {
+        if (err) {
+          client.close();
+          callback(err);
+          return;
+        }
+        console.log('Result of find:\n', docs);
+        client.close();
+        callback(err);
+      });
+    });
+  });
+}
 
 async function testWithAsync() {
-    console.log('\n-- testWithAsync ---');
-    const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
-    try {
-        await client.connect();
-        console.log('Connected to MongoDB');
-        const db = client.db();
-        const collection = db.collection('employees');
-        const employee = { id: 2, name: 'B. Async', age: 16 };
-        const result = await collection.insertOne(employee);
-        console.log('Result of inset:\n', result.insertedId);
-        const docs = await collection.find({_id: result.insertedId}).toArray();
-        console.log('Result of find:\n', docs)
-    } catch(err) {
-        console.log(err);
-    } finally {
-        client.close();
-    }
+  console.log('\n--- testWithAsync ---');
+  const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB URL', url);
+    const db = client.db();
+    const collection = db.collection('employees');
+
+    const employee = { id: 2, name: 'B. Async', age: 16 };
+    const result = await collection.insertOne(employee);
+    console.log('Result of insert:\n', result.insertedId);
+
+    const docs = await collection.find({ _id: result.insertedId })
+      .toArray();
+    console.log('Result of find:\n', docs);
+  } catch(err) {
+    console.log(err);
+  } finally {
+    client.close();
+  }
 }
+
+testWithCallbacks(function(err) {
+  if (err) {
+    console.log(err);
+  }
+  testWithAsync();
+});
