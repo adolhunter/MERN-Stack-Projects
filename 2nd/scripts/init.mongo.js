169855c17fc9a27aa973d87b854b1ad95747167a
adolhunter <mingshiwang123@gmail.com>
2020-04-03 21:34:01 -0500

    add init.mongo.js

---
+++ 2nd/scripts/init.mongo.js
@@ -0,0 +1,21 @@
+db.issues.remove({});
+const issueDB = [
+    {
+        id: 1, status: 'New', owner: 'Ravan', effort: 5,
+        created: new Date('01/16/2019'), due: undefined,
+        title: 'Error in console when clicking add'
+    }, {
+        id: 2, status: 'Assigned', owner: 'Eddie', effort: 14,
+        created: new Date('01/26/2019'), due: new Date('02/01/2019'),
+        title: 'Missing bottom border'
+    }
+];
+
+db.issues.insertMany(issueDB);
+const count = db.issues.count();
+print('Inserted', count, 'issues');
+
+db.issues.createIndex({id: 1}, {unique: true});
+db.issues.createIndex({status: 1});
+db.issues.createIndex({owner: 1});
+db.issues.createIndex({created: 1});
