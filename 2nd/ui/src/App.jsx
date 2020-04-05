1be9b7074e4694e2410a8b5a394d4b61b3cf1216
adolhunter <mingshiwang123@gmail.com>
2020-04-05 03:42:58 -0500

    refractor ui server files using eslint

--- 2nd/ui/src/App.jsx
+++ 2nd/ui/src/App.jsx
@@ -1,18 +1,25 @@
-const dateRegex = new RegExp("^\\d\\d\\d\\d-\\d\\d-\\d\\d");
+/* eslint "react/react-in-jsx-scope": "off" */
+/* globals React ReactDOM PropTypes */
+/* eslint-disable max-classes-per-file */
+/* eslint "react/no-multi-comp": "off" */
+/* eslint "no-alert": "off" */
 
-function jsonDateReceiver(key, value) {
+
+const dateRegex = new RegExp('^\\d\\d\\d\\d-\\d\\d-\\d\\d');
+
+function jsonDateReviver(key, value) {
   if (dateRegex.test(value)) return new Date(value);
   return value;
 }
 
+// eslint-disable-next-line react/prefer-stateless-function
 class IssueFilter extends React.Component {
   render() {
     return <div>This is a placeholder for the issue filter.</div>;
   }
 }
 
-function IssueRow(props) {
-  const issue = props.issue;
+function IssueRow({ issue }) {
   return (
     <tr>
       <td>{issue.id}</td>
@@ -20,14 +27,14 @@ function IssueRow(props) {
       <td>{issue.owner}</td>
       <td>{issue.created.toDateString()}</td>
       <td>{issue.effort}</td>
-      <td>{issue.due ? issue.due.toDateString() : ""}</td>
+      <td>{issue.due ? issue.due.toDateString() : ''}</td>
       <td>{issue.title}</td>
     </tr>
   );
 }
 
-function IssueTable(props) {
-  const issueRows = props.issues.map(issue => (
+function IssueTable({ issues }) {
+  const issueRows = issues.map((issue) => (
     <IssueRow key={issue.id} issue={issue} />
   ));
 
@@ -49,30 +56,6 @@ function IssueTable(props) {
   );
 }
 
-/* class IssueTable extends React.Component {
-    render() {
-        const issueRows = this.props.issues.map(issue => <IssueRow key={issue.id} issue={issue} />);
-        return (
-            <table className="bordered-table">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Status</th>
-                        <th>Owner</th>
-                        <th>Created</th>
-                        <th>Effort</th>
-                        <th>Due Date</th>
-                        <th>Title</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {issueRows}
-                </tbody>
-            </table>
-        );
-    }
-} */
-
 class IssueAdd extends React.Component {
   constructor() {
     super();
@@ -85,11 +68,13 @@ class IssueAdd extends React.Component {
     const issue = {
       owner: form.owner.value,
       title: form.title.value,
-      due: new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 10)
+      due: new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 10),
     };
-    this.props.createIssue(issue);
-    form.owner.value = "";
-    form.title.value = "";
+    // eslint-disable-next-line react/prop-types
+    const { createIssue } = this.props;
+    createIssue(issue);
+    form.owner.value = '';
+    form.title.value = '';
   }
 
   render() {
@@ -97,34 +82,39 @@ class IssueAdd extends React.Component {
       <form name="issueAdd" onSubmit={this.handleSubmit}>
         <input type="text" name="owner" placeholder="Owner" />
         <input type="text" name="title" placeholder="Title" />
-        <button>Add</button>
+        <button type="submit">Add</button>
       </form>
     );
   }
 }
 
+IssueAdd.propTypes = {
+  createIssue: PropTypes.func.isRequired,
+};
+
 async function graphQLFetch(query, variables = {}) {
   try {
     const response = await fetch(window.ENV.UI_API_ENDPOINT, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ query, variables })
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ query, variables }),
     });
     const body = await response.text();
-    const result = JSON.parse(body, jsonDateReceiver);
+    const result = JSON.parse(body, jsonDateReviver);
 
     if (result.errors) {
       const error = result.errors[0];
-      if (error.extensions.code == "BAD_USER_INPUT") {
-        const details = error.extensions.exception.errors.join("\n");
-        alert(`${error.message}:\n${details}`);
+      if (error.extensions.code === 'BAD_USER_INPUT') {
+        const details = error.extensions.exception.errors.join('\n ');
+        alert(`${error.message}:\n ${details}`);
       } else {
-        alert(`${error.extensions.code}:${error.message}`);
+        alert(`${error.extensions.code}: ${error.message}`);
       }
     }
     return result.data;
   } catch (e) {
-    alert(`Error in sending data to the server: ${e.message}`);
+    alert(`Error in sending data to server: ${e.message}`);
+    return null;
   }
 }
 
@@ -141,11 +131,11 @@ class IssueList extends React.Component {
 
   async loadData() {
     const query = `query {
-            issueList{
-                id title status owner 
-                created effort due
-            }
-        }`;
+      issueList {
+        id title status owner
+        created effort due
+      }
+    }`;
 
     const data = await graphQLFetch(query);
     if (data) {
@@ -154,11 +144,12 @@ class IssueList extends React.Component {
   }
 
   async createIssue(issue) {
-    const query = `mutation issueAdd($issue:IssueInputs!) {
-        issueAdd(issue: $issue) {
-            id
-        }
+    const query = `mutation issueAdd($issue: IssueInputs!) {
+      issueAdd(issue: $issue) {
+        id
+      }
     }`;
+
     const data = await graphQLFetch(query, { issue });
     if (data) {
       this.loadData();
@@ -166,19 +157,20 @@ class IssueList extends React.Component {
   }
 
   render() {
+    const { issues } = this.state;
     return (
-      <React.Fragment>
+      <>
         <h1>Issue Tracker</h1>
         <IssueFilter />
         <hr />
-        <IssueTable issues={this.state.issues} />
+        <IssueTable issues={issues} />
         <hr />
         <IssueAdd createIssue={this.createIssue} />
-      </React.Fragment>
+      </>
     );
   }
 }
 
 const element = <IssueList />;
 
-ReactDOM.render(element, document.getElementById("contents"));
+ReactDOM.render(element, document.getElementById('contents'));
