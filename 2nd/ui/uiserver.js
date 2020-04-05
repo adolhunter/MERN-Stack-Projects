af8e79b8ed2542a078213ef2b3aa3d3a4d0dcbbb
adolhunter <mingshiwang123@gmail.com>
2020-04-04 23:31:41 -0500

    try proxy server and keep local

--- 2nd/ui/uiserver.js
+++ 2nd/ui/uiserver.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 require('dotenv').config();
 
 const app = express();
@@ -7,6 +8,11 @@ app.use(express.static('public'));
 
 const UI_API_ENDPOINT = process.env.UI_API_ENDPOINT || 'http://localhost:3000/graphql';
 const env = { UI_API_ENDPOINT };
+const apiProxyTarget = process.env.API_PROXY_TARGET;
+
+if (apiProxyTarget) {
+    app.use('/graphql', createProxyMiddleware({ target: apiProxyTarget, changeOrigin: true}));
+}
 
 app.get('/env.js', (req, res) => {
     res.send(`window.ENV = ${JSON.stringify(env)}`);
